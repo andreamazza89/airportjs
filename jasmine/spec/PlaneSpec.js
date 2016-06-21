b9d2aa890498b5c3c9271698b2fb5f0443098d22
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-06-21 17:09:23 +0100

    Refactor Plane spec

--- jasmine/spec/PlaneSpec.js
+++ jasmine/spec/PlaneSpec.js
@@ -1,55 +1,30 @@
 'use strict';
 
 describe('Plane',function(){
-
-  beforeEach(function(){
     var plane;
     var weather;
     var airport;
-    var weather = new Weather;
+
+  beforeEach(function(){
+    weather = new Weather;
     spyOn(weather, 'isStormy').and.returnValue(false)
-    var plane = new Plane(weather);
+    plane = new Plane(weather);
     airport = jasmine.createSpyObj('airport',['clearForLanding','clearForTakeOff']);
   });
 
   it('can land at an airport', function(){
-
-    var plane;
-    var weather;
-    var airport;
-    var weather = new Weather;
-    spyOn(weather, 'isStormy').and.returnValue(false)
-    var plane = new Plane(weather);
-    airport = jasmine.createSpyObj('airport',['clearForLanding','clearForTakeOff']);
-
     plane.land(airport);
     expect(airport.clearForLanding).toHaveBeenCalledWith(plane);
   });
 
   it('can take off from an airport', function(){
-
-    var plane;
-    var weather;
-    var airport;
-    var weather = new Weather;
-    spyOn(weather, 'isStormy').and.returnValue(false)
-    var plane = new Plane(weather);
-    airport = jasmine.createSpyObj('airport',['clearForLanding','clearForTakeOff']);
-
     plane.land(airport);
     plane.takeOff();
     expect(airport.clearForTakeOff).toHaveBeenCalledWith(plane);
   })
 
   it('cannot land at an airport if Weather is stormy', function(){
-    var plane;
-    var weather;
-    var airport;
-    var weather = new Weather;
-    spyOn(weather, 'isStormy').and.returnValue(true)
-    var plane = new Plane(weather);
-    airport = jasmine.createSpyObj('airport',['clearForLanding','clearForTakeOff']);
-
+    weather.isStormy = jasmine.createSpy().and.returnValue(true);
     expect(function() { plane.land(airport) }).toThrow(new Error('Too stormy to land'));
   });
 });
